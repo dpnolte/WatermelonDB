aba2c662e1024ce15b1076ab0f973a05116ff53a
Derk <dpnolte@gmail.com>
2020-07-10 15:46:32 +0200

    using dist as root

--- native/iosTest/WatermelonTesterTests/Tests.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import XCTest
-
-// Always add new tests here to ensure they get compiled
-private let allTests: [XCTest.Type] = [
-    /* WatermelonDB */
-    DatabaseDriverTests.self,
-    BridgeTests.self,
-]
